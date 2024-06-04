921dbf8cff3ba343700a6b728c86308ab1670734
abinpramudya <abinpramdya2@gmail.com>
2024-06-04 20:31:13 +0700

    initial new commit

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -26,33 +26,33 @@ function shapecoef_cal(LoS,LWL,LPP,cb,B,T,TA,TF,Dprop,Nrudder){
   Nbossing = Nrudder
 
   if (Nrudder === 1){
-    s0 = -0.6837
-    s1 = 0.2771
-    s2 = 0.6542
-    s3 = 0.6422
-    s4 = 0.0075
-    s5 = 0.0275
-    s6 = -0.0045
-    s7 = -0.4798
-    s8 = 0.0376
-    Krudder = 0
-    Kbracket = 0
-    Kbossing = 0
+    var s0 = -0.6837
+    var s1 = 0.2771
+    var s2 = 0.6542
+    var s3 = 0.6422
+    var s4 = 0.0075
+    var s5 = 0.0275
+    var s6 = -0.0045
+    var s7 = -0.4798
+    var s8 = 0.0376
+    var  Krudder = 0
+    var  Kbracket = 0
+    var  Kbossing = 0
   }
   
   if (Nrudder === 2){
-    s0 = -0.4319
-    s1 = 0.1685
-    s2 = 0.5637
-    s3 = 0.5891
-    s4 = 0.0033
-    s5 = 0.0134
-    s6 = -0.0005
-    s7 = -2.7932
-    s8 = 0.0072
-    Krudder = 0.0131
-    Kbracket = -0.003
-    Kbossing = 0.0061
+    var s0 = -0.4319
+    var s1 = 0.1685
+    var s2 = 0.5637
+    var s3 = 0.5891
+    var s4 = 0.0033
+    var s5 = 0.0134
+    var s6 = -0.0005
+    var s7 = -2.7932
+    var s8 = 0.0072
+    var Krudder = 0.0131
+    var  Kbracket = -0.003
+    var Kbossing = 0.0061
   }
 
   k = s0 + (s1 * (LoS / LWL)) + (s2 * (LWL / LPP)) + (s3 * cb) + (s4 * (LPP / B)) +
@@ -73,27 +73,27 @@ function resistancefriction_cal(cf,p,s,v){
 
 function coefficientFrictionResidual_cal(froude_number,coefficientblock,Nrudder){
   if (Nrudder === 1){
-    b11 = -0.57424
-    b12 = 13.3893
-    b13 = 90.596
-    b21 = 4.6614
-    b22 = -39.721
-    b23 = -351.483
-    b31 = -1.14215
-    b32 = -12.3296
-    b33 = 459.254
+    var b11 = -0.57424
+    var b12 = 13.3893
+    var b13 = 90.596
+    var b21 = 4.6614
+    var b22 = -39.721
+    var b23 = -351.483
+    var b31 = -1.14215
+    var  b32 = -12.3296
+    var b33 = 459.254
   }
 
   if (Nrudder === 2){
-    b11 = -5.3475
-    b12 = 55.6532
-    b13 = -114.905
-    b21 = 19.2714
-    b22 = -192.388
-    b23 = 388.333
-    b31 = -14.3571
-    b32 = 142.738
-    b33 = -254.762
+    var b11 = -5.3475
+    var b12 = 55.6532
+    var b13 = -114.905
+    var b21 = 19.2714
+    var b22 = -192.388
+    var b23 = 388.333
+    var b31 = -14.3571
+    var b32 = 142.738
+    var b33 = -254.762
   }
   
   return b11 + (b12 * froude_number) + (b13 * (froude_number ** 2)) +
@@ -103,169 +103,167 @@ function coefficientFrictionResidual_cal(froude_number,coefficientblock,Nrudder)
 
 function fcrit_cal(Nrudder,coefficientblock){
   if (Nrudder === 1){
-    d1 = 0.854
-    d2 = -1.228
-    d3 = 0.497
+    var d1 = 0.854
+    var d2 = -1.228
+    var d3 = 0.497
   }
 
   if (Nrudder === 2){
-    d1 = 0.897
-    d2 = -1.457
-    d3 = 0.767
+    var d1 = 0.897
+    var d2 = -1.457
+    var d3 = 0.767
   }
   return d1 + (d2 * coefficientblock) + (d3 *(coefficientblock ** 2))
 }
 
 function kfr_cal(fcrit,fn){
   if(fn < fcrit){
-    a = 1
+    var a = 1
   }
   if(fn > fcrit){
     c1 = fn / fcrit
-    a = (fn/fcrit) ** c1
+    var a = (fn/fcrit) ** c1
   }
   return a
 }
 
-function lengthfactor_cal(LPP,Nprop){
-  let e1;
-  let e2;
-  if (Nprop === 1){
-    e1 = 2.1701
-    e2 = -0.1602
+function lengthfactor_cal(LPP,Nrudder){
+
+  if (Nrudder === 1){
+    var e1 = 2.1701
+    var e2 = -0.1602
   }
 
-  if (Nprop === 2){
-    e1 = 1.8319
-    e2 = -0.1237
+  if (Nrudder === 2){
+    var e1 = 1.8319
+    var e2 = -0.1237
   }
   console.log('e1',e1)
   return e1 * (LPP ** e2)
 }
 
-function beamdraft_cal(B,T,Nprop){
-  let a;
+function beamdraft_cal(B,T,Nrudder){
   let a1;
-  if (Nprop === 1){
+  if (Nrudder === 1){
     a1 =0.3382
   }
 
-  if (Nprop ===2){
+  if (Nrudder ===2){
     a1 = 0.2748
   }
   ratio = B/T
 
   if (ratio < 1.99){
-    a  = 1.99 ** a1
+    var  a  = 1.99 ** a1
   }
 
   if (ratio >= 1.99){
-    a = (B/T) ** a1
+    var a = (B/T) ** a1
   }
   return a
 }
 
-function lengthbeam_cal(LPP,B,Nprop){
+function lengthbeam_cal(LPP,B,Nrudder){
   let a2;
-  if (Nprop === 1){
+  if (Nrudder === 1){
     a2 = -0.8086
   }
 
-  if (Nprop ===2){
+  if (Nrudder ===2){
     a2 = -0.5747
   }
   ratio = LPP/B
 
   if (ratio <= 7.11){
-    a  = (LPP/B) ** a2
+    var  a  = (LPP/B) ** a2
   }
 
   if (ratio > 7.11){
-    a = 7.11 **a2
+    var a = 7.11 **a2
   }
 
   return a
 }
 
-function wettedratio_cal(LoS,LWL,Nprop){
+function wettedratio_cal(LoS,LWL,Nrudder){
   let a3;
-  if (Nprop === 1){
+  if (Nrudder === 1){
     a3 = -6.0258
   }
 
-  if (Nprop ===2){
+  if (Nrudder ===2){
     a3 = -6.761
   }
 
   ratio = LoS / LWL
 
   if (ratio <= 1.05){
-    a  = (LoS/LWL) ** a3
+    var   a  = (LoS/LWL) ** a3
   }
 
   if (ratio > 1.05){
-    a = 1.05 **a3
+    var  a = 1.05 **a3
   }
   return a
 }
 
-function aftoverhang_cal(LWL,LPP,Nprop){
+function aftoverhang_cal(LWL,LPP,Nrudder){
   let a4;
-  if (Nprop === 1){
+  if (Nrudder === 1){
     a4 = -3.5632
   }
 
-  if (Nprop ===2){
+  if (Nrudder ===2){
     a4 = -4.3834
   }
 
   ratio = LWL / LPP
 
   if (ratio <= 1.06){
-    a  = ratio ** a4
+    var   a  = ratio ** a4
   }
 
   if (ratio > 1.05){
-    a = 1.06 **a4
+    var   a = 1.06 **a4
   }
   return a
 }
 
-function trimcorrection_cal(TA,TF,Nprop,LPP){
+function trimcorrection_cal(TA,TF,Nrudder,LPP){
   let a5;
-  if (Nprop === 1){
+  if (Nrudder === 1){
     a5 = 9.4405
   }
 
-  if (Nprop ===2){
+  if (Nrudder ===2){
     a5 = 8.8158
   }
 
   return (1 + ((TA - TF) / LPP)) ** a5
 }
 
-function propellerfactor_cal(D,TA,Nprop){
+function propellerfactor_cal(D,TA,Nrudder){
   let a6;
-  if (Nprop === 1){
+  if (Nrudder === 1){
     a6 = 0.0146
   }
 
-  if (Nprop ===2){
+  if (Nrudder ===2){
     a6 = -0.1418
   }
 
   ratio = D/TA
 
   if (ratio < 0.43){
-  a = 0.43 ** a6
+    var  a = 0.43 ** a6
   }
 
   if (ratio >= 0.43 && ratio <= 0.84){
-  a = (ratio) **a6
+    var  a = (ratio) **a6
   }
 
   if (ratio > 0.84){
-  a = 0.84 ** a6
+    var a = 0.84 ** a6
   }
 
   return a
@@ -274,22 +272,18 @@ function propellerfactor_cal(D,TA,Nprop){
 function coefresidual_cal(Nthruster,Nrudder,crstd,kfr,lengthfactor,beamdraft,lengthbeam,
                            wettedratio,aftoverhang,trimcorrection,propellerfactor){
   
-   let a7;
-   let a8;
-   let a9;
-   let a10;
-  if (Nprop === 1){
-    a7 = 0
-    a8 = 0
-    a9 = 0
-    a10 = 0
+  if (Nrudder === 1){
+    var a7 = 0
+    var a8 = 0
+    var a9 = 0
+    var a10 = 0
   }
 
-  if (Nprop ===2){
-    a7 = -0.1258
-    a8 = 0.0481
-    a9 = 0.1699
-    a10 = 0.0728
+  if (Nrudder ===2){
+    var a7 = -0.1258
+    var a8 = 0.0481
+    var a9 = 0.1699
+    var a10 = 0.0728
   }
   Nthruster = 1
   Nbracket = Nrudder
@@ -352,6 +346,7 @@ function route_def(route){
   days = 365
   avail = days - docking
 
+  //SUB-WIN-DIL-SUB
   if (route === 1){
     var distance = 1630	
     var sailingTime = 118.1	
@@ -488,61 +483,7 @@ function route_def(route){
   
 
 }
-// function FinalFO_cal()
-
-function getForm(){
-  event.preventDefault();
-  const shipData = document.getElementById("ship-data-form")
-
-  // Reading input values using JavaScript
-  // const Name = shipData.querySelector('input[name="Vessel-Name"]').value;
-  // const VesselType = shipData.querySelector('select[name="vessel-type"]').value;
-  // const LOA = parseFloat(shipData.querySelector('input[name="LOA"]').value);
-  // const LPP = parseFloat(shipData.querySelector('input[name="LPP"]').value);
-  // const LWL = LPP * 1.035
-  // const LoS = parseFloat(shipData.querySelector('input[name="LoS"]').value);
-  // const B = parseFloat(shipData.querySelector('input[name="B"]').value);
-  // const T = parseFloat(shipData.querySelector('input[name="T"]').value);
-  // const H = parseFloat(shipData.querySelector('input[name="H"]').value);
-  // const DWT = parseFloat(shipData.querySelector('input[name="DWT"]').value);
-  // const GT = parseFloat(shipData.querySelector('input[name="GT"]').value);
-  // const NT = parseFloat(shipData.querySelector('input[name="NT"]').value);
-  // const Displacement = parseFloat(shipData.querySelector('input[name="Displacement"]').value);
-  // const Vs = parseFloat(shipData.querySelector('input[name="Vs"]').value);
-  // const Dprop = parseFloat(shipData.querySelector('input[name="Dprop"]').value);
-  // const Nrudder = parseFloat(shipData.querySelector('input[name="Nrudder"]').value);
-  // const Nthruster = parseFloat(shipData.querySelector('input[name="Nthruster"]').value);
-  // const TA = parseFloat(shipData.querySelector('input[name="TA"]').value);
-  // const TF = parseFloat(shipData.querySelector('input[name="TF"]').value);
-
-  //dummy values
-  const Name = "Jaladhimantri"
-  const VesselType = "General Cargo"
-  const LOA = 117
-  const LPP = 110
-  const LWL = LPP * 1.035
-  const LoS = 114
-  const B = 19.7
-  const T = 6.45
-  const H = 8.5
-  const DWT = 7664.6
-  const Displacement = 11129.6
-  const Vs = 13.8
-  const Dprop = 3.7
-  const Nrudder = 1
-  const Nthruster = 0
-  const Nprop = 1
-  const TA = 4
-  const TF = 5
-
-  const pme = 3200
-  const sfoc = 182
-  const rpm = 168.717948717949
-  const Pservice = 0.75
-
-  route = 1
-  
-  //var name after calculation
+function FinalFO_cal(name, vesselType, LOA, LPP, LWL, LoS, B, T, H,Vs, Dprop, Nrudder, Nthruster, Nrudder, TA, TF, pme, sfoc, rpm, pservice){
   var reynold_number = reynoldnumber(Vs,LWL);
   var cf_number = coefficientfriction(reynold_number);
   var froude_number = froudenumber(Vs,LWL);
@@ -553,13 +494,13 @@ function getForm(){
   var crstd = coefficientFrictionResidual_cal(froude_number,coefficientblock,Nrudder)
   var fcrit = fcrit_cal(Nrudder, coefficientblock)
   var kfr = kfr_cal(fcrit,froude_number)
-  var lengthfactor = lengthfactor_cal(LPP,Nprop)
-  var Beamdraft= beamdraft_cal(B,T,Nprop)
-  var lengthbeam=lengthbeam_cal(LPP,B,Nprop)
-  var wettedratio = wettedratio_cal(LoS,LWL,Nprop)
-  var aftoverhang = aftoverhang_cal(LWL,LPP,Nprop)
-  var trimcorrection = trimcorrection_cal(TA,TF,Nprop,LPP)
-  var propellerfactor = propellerfactor_cal(Dprop,TA,Nprop)
+  var lengthfactor = lengthfactor_cal(LPP,Nrudder)
+  var Beamdraft= beamdraft_cal(B,T,Nrudder)
+  var lengthbeam=lengthbeam_cal(LPP,B,Nrudder)
+  var wettedratio = wettedratio_cal(LoS,LWL,Nrudder)
+  var aftoverhang = aftoverhang_cal(LWL,LPP,Nrudder)
+  var trimcorrection = trimcorrection_cal(TA,TF,Nrudder,LPP)
+  var propellerfactor = propellerfactor_cal(Dprop,TA,Nrudder)
   var coefficientresidual = coefresidual_cal(Nthruster,Nrudder,crstd,kfr,
                                              lengthfactor,Beamdraft,lengthbeam,
                                              wettedratio,aftoverhang,trimcorrection,
@@ -575,11 +516,13 @@ function getForm(){
   var Sfoc = SFOC_cal(sfocrel,sfoc)
   var FOC = FOC_cal(pme)
 
+  
   var gfoc = FOC * Sfoc
   var kgfoc = gfoc / 1000
   var tfoc = gfoc / 1000000
   var LFO = tfoc / 0.991
   var MDO = tfoc / 0.89
+
 
   //logging
   console.log('reynold_number:',reynold_number)
@@ -616,11 +559,108 @@ function getForm(){
   console.log("LFO:", LFO);
   console.log("MDO:", MDO);
 
-  
-  // Store result in local storage
-  localStorage.setItem('reynold_number', reynold_number);
-  localStorage.setItem('cf_number',cf_number)
-  localStorage.setItem('froude_number',froude_number)
-  // window.location.href = 'results.html';
 
+  // Store the values in local storage
+localStorage.setItem('reynold_number', reynold_number);
+localStorage.setItem('cf_number', cf_number);
+localStorage.setItem('froude_number', froude_number);
+localStorage.setItem('coefficientblock', coefficientblock);
+localStorage.setItem('shapecoef', shapecoef);
+localStorage.setItem('wetsurface', wetsurface);
+localStorage.setItem('resistancefriction', resistancefriction);
+localStorage.setItem('crstd', crstd);
+localStorage.setItem('fcrit', fcrit);
+localStorage.setItem('kfr', kfr);
+localStorage.setItem('lengthfactor', lengthfactor);
+localStorage.setItem('Beamdraft', Beamdraft);
+localStorage.setItem('lengthbeam', lengthbeam);
+localStorage.setItem('wettedratio', wettedratio);
+localStorage.setItem('aftoverhang', aftoverhang);
+localStorage.setItem('trimcorrection', trimcorrection);
+localStorage.setItem('propellerfactor', propellerfactor);
+localStorage.setItem('coefficientresidual', coefficientresidual);
+localStorage.setItem('resistanceresidual', resistanceresidual);
+localStorage.setItem('resistancetotal', resistancetotal);
+localStorage.setItem('power_propel', power_propel);
+localStorage.setItem('propeff', propeff);
+localStorage.setItem('totalpower', totalpower);
+localStorage.setItem('noe', noe);
+localStorage.setItem('el', el);
+localStorage.setItem('sfocrel', sfocrel);
+localStorage.setItem('Sfoc', Sfoc);
+localStorage.setItem('FOC', FOC);
+localStorage.setItem('gfoc', gfoc);
+localStorage.setItem('kgfoc', kgfoc);
+localStorage.setItem('tfoc', tfoc);
+localStorage.setItem('LFO', LFO);
+localStorage.setItem('MDO', MDO);
 }
+
+function getForm(){
+  event.preventDefault();
+  const shipData = document.getElementById("ship-data-form")
+  const MachineryData = document.getElementById("machinery-data-form")
+  const RouteData = document.getElementById("route-selection-form")
+
+  // Reading input values using JavaScript
+  const Name = shipData.querySelector('input[name="Vessel-Name"]').value;
+  const VesselType = shipData.querySelector('select[name="vessel-type"]').value;
+  const LOA = parseFloat(shipData.querySelector('input[name="LOA"]').value);
+  const LPP = parseFloat(shipData.querySelector('input[name="LPP"]').value);
+  const LWL = LPP * 1.035
+  const LoS = parseFloat(shipData.querySelector('input[name="LoS"]').value);
+  const B = parseFloat(shipData.querySelector('input[name="B"]').value);
+  const T = parseFloat(shipData.querySelector('input[name="T"]').value);
+  const H = parseFloat(shipData.querySelector('input[name="H"]').value);
+  const DWT = parseFloat(shipData.querySelector('input[name="DWT"]').value);
+  const Vs = parseFloat(shipData.querySelector('input[name="Vs"]').value);
+  const Dprop = parseFloat(shipData.querySelector('input[name="Dprop"]').value);
+  const Nrudder = parseFloat(shipData.querySelector('input[name="Nrudder"]').value);
+  const Nthruster = parseFloat(shipData.querySelector('input[name="Nthruster"]').value);
+  const TA = parseFloat(shipData.querySelector('input[name="TA"]').value);
+  const TF = parseFloat(shipData.querySelector('input[name="TF"]').value);
+  
+  const pme = parseFloat(MachineryData.querySelector('input[name="Pme"]').value);
+  const sfoc = parseFloat(MachineryData.querySelector('input[name="SFOC"]').value);
+  const rpm = parseFloat(MachineryData.querySelector('input[name="RPM"]').value);
+  const Pservice = parseFloat(MachineryData.querySelector('input[name="Pservice"]').value);
+
+  // const route = RouteData.querySelector('input[name="route"]').value;
+
+  FinalFO_cal(Name,VesselType,LOA,LPP,LWL,LoS,B,T,H,Vs,Dprop,Nrudder,Nthruster,Nrudder,TA,TF,pme,sfoc,rpm,Pservice)
+  window.location.href = 'results.html';
+}
+
+
+  //dummy values
+  // const Name = "Jaladhimantri"
+  // const VesselType = "General Cargo"
+  // const LOA = 117
+  // const LPP = 110
+  // const LWL = LPP * 1.035
+  // const LoS = 114
+  // const B = 19.7
+  // const T = 6.45
+  // const H = 8.5
+  // const DWT = 7664.6
+  // const Displacement = 11129.6
+  // const Vs = 13.8
+  // const Dprop = 3.7
+  // const Nrudder = 1
+  // const Nthruster = 0
+  // const Nrudder = 1
+  // const TA = 4
+  // const TF = 5
+
+  // const pme = 3200
+  // const sfoc = 182
+  // const rpm = 168.717948717949
+  // const Pservice = 0.75
+
+  // route = 1
+
+    
+  // Store result in local storage
+  // localStorage.setItem('reynold_number', reynold_number);
+  // localStorage.setItem('cf_number',cf_number)
+  // localStorage.setItem('froude_number',froude_number)
