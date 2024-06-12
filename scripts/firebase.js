afe68a487f4e8cd8f65f7bda2eafea9ff2bc70d2
abinpramudya <abinpramdya2@gmail.com>
2024-06-12 18:52:34 +0700

    update front end sign up page

---
+++ scripts/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyC9NtpCxUv8-MmXpIPOqBjkytAgvLOlfKo",
+authDomain: "cii-website-calculator.firebaseapp.com",
+projectId: "cii-website-calculator",
+storageBucket: "cii-website-calculator.appspot.com",
+messagingSenderId: "141598285360",
+appId: "1:141598285360:web:5bdd92f46c4d5f8b8c97f5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
